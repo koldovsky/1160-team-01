819809a80cc0dd04a6404f7cf38dc6453099fd32
bestsilk251 <46943511+bestsilk251@users.noreply.github.com>
2024-03-18 19:11:16 +0000

    changed js tasks files

--- js-tasks/task-03.js
+++
@@ -1,6 +0,0 @@
-// Game Move
-//https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
-
-function move(position, roll) {
-  return position + roll * 2;
-}
--- js-tasks/task-05.js
+++
@@ -1,6 +0,0 @@
-//Keep Hydrated
-//https://www.codewars.com/kata/keep-hydrated-1/train/javascript
-
-function litres(time) {
-  return Math.floor(time * 0.5);
-}
