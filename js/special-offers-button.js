48ffb84212baf6b67c1248a2b4ee92251e834fb9
karolinabalukh <154453086+karolinabalukh@users.noreply.github.com>
2024-03-24 20:45:56 +0000

    update special-offers css and add js

---
+++ js/special-offers-button.js
@@ -0,0 +1,8 @@
+let contacts__button = document.getElementById("contacts__button");
+
+contacts__button.addEventListener('mousemove', (e) => {
+    x = e.offsetX;
+    y = e.offsetY;
+    contacts__button.style.setProperty('--mouse-x', x + "px");
+    contacts__button.style.setProperty('--mouse-y', y + "px");
+});
