0609395e6a6dd7c14e82609312d5870293699a1c
Vyacheslav Koldovskyy <koldovsky@koldovsky.com>
2024-03-25 15:29:32 +0000

    Mouse event rename

--- js/special-offers-button.js
+++ js/special-offers-button.js
@@ -1,10 +1,10 @@
 const contactsButtons = document.querySelectorAll(".contacts__button");
 
 contactsButtons.forEach((button) => {
-  button.addEventListener("mousemove", (e) => {
-    const x = e.offsetX;
-    const y = e.offsetY;
-    e.target.style.setProperty("--mouse-x", x + "px");
-    e.target.style.setProperty("--mouse-y", y + "px");
+  button.addEventListener("mousemove", (event) => {
+    const x = event.offsetX;
+    const y = event.offsetY;
+    event.target.style.setProperty("--mouse-x", x + "px");
+    event.target.style.setProperty("--mouse-y", y + "px");
   });
 });
