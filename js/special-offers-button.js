1197daf7fb7ce0f993ba3911f628b503e358e987
karolinabalukh <154453086+karolinabalukh@users.noreply.github.com>
2024-03-25 12:03:50 +0000

    edit css

--- js/special-offers-button.js
+++ js/special-offers-button.js
@@ -1,4 +1,4 @@
-let contacts__button = document.getElementById("contacts__button");
+const contacts__button = document.getElementById("contacts__button");
 
 contacts__button.addEventListener('mousemove', (e) => {
     x = e.offsetX;
