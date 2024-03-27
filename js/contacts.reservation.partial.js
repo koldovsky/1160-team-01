1395d6e9ed4ec964a43ace6d3e576073e48e7e89
vladimircity <48383094+vladimircity@users.noreply.github.com>
2024-03-27 09:52:34 +0000

    renderContent

---
+++ js/contacts.reservation.partial.js
@@ -0,0 +1,22 @@
+const response = await fetch("api/reservation-content.json");
+const contentArr = await response.json();
+
+function renderContent(contentArr) {
+  let contentDomString = "";
+  for (const card of contentArr) {
+    contentDomString += `
+        <li class="book__li">
+            <div class="book__li-img">
+                <img src="${card.image}" alt="${card.alt}" />
+            </div>
+            <div class="book_li-content">
+                <p class="book__li-content-header">${card.title}</p>
+                <p class="book__text">${card.description}</p>
+            </div>
+        </li>`;
+  }
+  const contentContainer = document.querySelector(".book__ul");
+  contentContainer.innerHTML = contentDomString;
+}
+
+renderContent(contentArr);
