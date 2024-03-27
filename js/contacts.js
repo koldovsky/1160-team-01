e0d882c39a1132e27b003a943dc961c4af1e9b51
vladimircity <48383094+vladimircity@users.noreply.github.com>
2024-03-27 10:51:25 +0000

    loadedPartialsCount

--- js/contacts.js
+++ js/contacts.js
@@ -1,4 +1,12 @@
 function init() {
     import("./common.header.partial.js");
     import("./contacts.reservation.partial.js");
-  }
+}
+
+const totalPartials = document.querySelectorAll('[hx-trigger="load"], [data-hx-trigger="load"]').length;
+let loadedPartialsCount = 0;
+
+document.body.addEventListener('htmx:afterOnLoad', () => {
+    loadedPartialsCount++;
+    if (loadedPartialsCount === totalPartials) init();
+});
