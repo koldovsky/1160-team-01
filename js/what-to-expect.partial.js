814ef4bd90c9e05dccb113ac94773c767bf8f2a4
YarynaHum <160642842+YarynaHum@users.noreply.github.com>
2024-04-02 20:23:54 +0000

    remove id

--- js/what-to-expect.partial.js
+++ js/what-to-expect.partial.js
@@ -1,18 +1,15 @@
 const items = [
     {
-        "id": "1",
         "title": "Exceptional Quality of Food",
         "image": "img/what-to-expect/exeptional-qualitty-of-food-picture.webp",
         "description": "We take pride in offering our guests the highest quality of ingredients sourced from local farms and markets."
     },
     {
-        "id": "2",
         "title": "Varied Menu Selection",
         "image": "img/what-to-expect/varied-menu-selection-picture.webp",
         "description": "Our menu features a wide selection of dishes to suit every taste and preference. We have something for everyone."
     },
     {
-        "id": "3",
         "title": "Impeccable Service",
         "image": "img/what-to-expect/impeccable-service-picture.webp",
         "description": "Our staff is trained to provide attentive and personalized service to each and every guest, ensuring a memorable dining experience."
