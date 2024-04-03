2201436b54e03ddf7a3d5daebcc0aef66d640e73
bestsilk251 <46943511+bestsilk251@users.noreply.github.com>
2024-04-03 15:15:26 +0000

    finished header and footer

--- js/common.header.partial.js
+++ js/common.header.partial.js
@@ -4,6 +4,12 @@ const headerNavMenu = document.querySelector(".header__nav-menu");
 burgerMenu.addEventListener("click", () => {
   burgerMenu.classList.toggle("active");
   headerNavMenu.classList.toggle("active");
+
+  if (headerNavMenu.classList.contains("active")) {
+    disableScroll();
+  } else {
+    enableScroll();
+  }
 });
 
 document.querySelectorAll(".header__nav-list").forEach((n) =>
@@ -12,3 +18,18 @@ document.querySelectorAll(".header__nav-list").forEach((n) =>
     headerNavMenu.classList.remove("active");
   })
 );
+
+function disableScroll() {
+  const scrollY = window.scrollY;
+
+  document.body.style.position = "fixed";
+  document.body.style.top = `-${scrollY}px`;
+  document.body.style.width = "100%";
+}
+
+function enableScroll() {
+  const scrollY = parseInt(document.body.style.top || "0", 10);
+  document.body.style.position = "";
+  document.body.style.top = "";
+  document.body.style.width = "";
+}
